ef4bf114f2f378c6178a6e4971fbefa83a81d171
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-04-11 23:18:17 -0500

    feat: Add new secuencias

--- leds/SecuenciaSimpleLedsV2/SecuenciaSimpleLedsV2.ino
+++ leds/SecuenciaSimpleLedsV2/SecuenciaSimpleLedsV2.ino
@@ -15,10 +15,18 @@ int secuenciaUnicoLed[7][6][3] = {
     {{LOW, 0, 0}, {LOW, 1, 0}, {LOW, 2, 0}, {LOW, 3, 0}, {LOW, 4, 0}, {HIGH, 5, 200}},
     {{LOW, 0, 0}, {LOW, 1, 0}, {LOW, 2, 0}, {LOW, 3, 0}, {LOW, 4, 0}, {LOW, 5, 0}}};
 
+int secuenciaUnicoLedV2[2][12][3] = {
+    {{HIGH, 0, 200}, {LOW, 0, 0}, {HIGH, 1, 200}, {LOW, 1, 0}, {HIGH, 2, 200}, {LOW, 2, 0}, {HIGH, 3, 200}, {LOW, 3, 0}, {HIGH, 4, 200}, {LOW, 4, 0}, {HIGH, 5, 200}, {LOW, 5, 0}},
+    {{HIGH, 5, 200}, {LOW, 5, 0}, {HIGH, 4, 200}, {LOW, 4, 0}, {HIGH, 3, 200}, {LOW, 3, 0}, {HIGH, 2, 200}, {LOW, 2, 0}, {HIGH, 1, 200}, {LOW, 1, 0}, {HIGH, 0, 200}, {LOW, 0, 0}}};
+
+int secuenciaUnicoLedV3[2][12][3] = {
+    {{HIGH, 0, 200}, {LOW, 0, 0}, {HIGH, 1, 160}, {LOW, 1, 0}, {HIGH, 2, 120}, {LOW, 2, 0}, {HIGH, 3, 70}, {LOW, 3, 0}, {HIGH, 4, 50}, {LOW, 4, 0}, {HIGH, 5, 40}, {LOW, 5, 0}},
+    {{HIGH, 5, 40}, {LOW, 5, 0}, {HIGH, 4, 50}, {LOW, 4, 0}, {HIGH, 3, 70}, {LOW, 3, 0}, {HIGH, 2, 120}, {LOW, 2, 0}, {HIGH, 1, 160}, {LOW, 1, 0}, {HIGH, 0, 200}, {LOW, 0, 0}}};
+
 int arregloDePines[] = {D0, D1, D2, D3, D4, D5};
 int tamaño = sizeof(arregloDePines) / sizeof(arregloDePines[0]);
-int totalFilas = sizeof(secuenciaUnicoLed) / sizeof(secuenciaUnicoLed[0]);
-int totalColumnas = sizeof(secuenciaUnicoLed[0]) / sizeof(secuenciaUnicoLed[0][0]);
+int totalFilas = sizeof(secuenciaUnicoLedV3) / sizeof(secuenciaUnicoLedV3[0]);
+int totalColumnas = sizeof(secuenciaUnicoLedV3[0]) / sizeof(secuenciaUnicoLedV3[0][0]);
 
 void setup()
 {
@@ -36,9 +44,9 @@ void loop()
     for (int posColumna = 0; posColumna < totalColumnas; posColumna++)
     {
       // Estado puede ser "encendio" = HIGH ó "apagado" = LOW
-      int estado = secuenciaUnicoLed[posFila][posColumna][0];
-      int posicionLed = secuenciaUnicoLed[posFila][posColumna][1];
-      int tiempoEntreCambio = secuenciaUnicoLed[posFila][posColumna][2];
+      int estado = secuenciaUnicoLedV3[posFila][posColumna][0];
+      int posicionLed = secuenciaUnicoLedV3[posFila][posColumna][1];
+      int tiempoEntreCambio = secuenciaUnicoLedV3[posFila][posColumna][2];
 
       digitalWrite(arregloDePines[posicionLed], estado);
       delay(tiempoEntreCambio);

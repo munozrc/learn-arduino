f11347fb327dbf990625d7a06f98cffc5291dade
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-05-01 18:00:31 -0500

    feat: update new example

--- games/snake/snake.ino
+++ games/snake/snake.ino
@@ -9,28 +9,53 @@ struct Pin
   static const short CLK = 9;
 };
 
+struct Coordinate
+{
+  int x = 0, y = 0;
+  Coordinate(int x = 0, int y = 0) : x(x), y(y) {}
+};
+
+Coordinate joystickOrigin(500, 500);
 LedControl matrix(Pin::DIN, Pin::CLK, Pin::CS, 1);
 
 void setup()
 {
   Serial.begin(9600);
+  joystickOrigin = calibrateAxis(Pin::JoystickX, Pin::JoystickY);
+  Serial.println("Nueva calibracion --> x = " + String(joystickOrigin.x, DEC) + " y = " + String(joystickOrigin.y, DEC));
 }
 
 void loop()
 {
-  int x = getAxisValue(Pin::JoystickX, 500, 160);
-  int y = getAxisValue(Pin::JoystickY, 500, 160);
+  int x = getAxisValue(Pin::JoystickX, joystickOrigin.x, 160);
+  int y = getAxisValue(Pin::JoystickY, joystickOrigin.y, 160);
 
   Serial.println("x = " + String(x, DEC) + " y = " + String(y, DEC));
 }
 
-int getAxisValue(uint8_t pin, int joystickHome, int joystickThreshold)
+int getAxisValue(uint8_t pin, int originAxis, int axisThreshold)
 {
   int value = 0;
   int raw = analogRead(pin);
 
-  raw < joystickHome - joystickThreshold ? value = 1 : 0;
-  raw > joystickHome + joystickThreshold ? value = -1 : 0;
+  raw < originAxis - axisThreshold ? value = 1 : 0;
+  raw > originAxis + axisThreshold ? value = -1 : 0;
 
   return value;
+}
+
+Coordinate calibrateAxis(uint8_t pinAxisX, uint8_t pinAxisY)
+{
+  Coordinate values;
+
+  for (int i = 0; i < 10; i++)
+  {
+    values.x += analogRead(pinAxisX);
+    values.y += analogRead(pinAxisY);
+  }
+
+  values.x = values.x / 10;
+  values.y = values.y / 10;
+
+  return values;
 }

d69b0e7b80326727d8c89135ec1a28246764c15a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-05-01 17:38:52 -0500

    feat: Updatet new example snake

--- games/snake/snake.ino
+++ games/snake/snake.ino
@@ -4,7 +4,6 @@ struct Pin
 {
   static const short JoystickX = A0;
   static const short JoystickY = A1;
-
   static const short DIN = 11;
   static const short CS = 10;
   static const short CLK = 9;
@@ -19,5 +18,19 @@ void setup()
 
 void loop()
 {
-  Serial.println("Hi!");
+  int x = getAxisValue(Pin::JoystickX, 500, 160);
+  int y = getAxisValue(Pin::JoystickY, 500, 160);
+
+  Serial.println("x = " + String(x, DEC) + " y = " + String(y, DEC));
+}
+
+int getAxisValue(uint8_t pin, int joystickHome, int joystickThreshold)
+{
+  int value = 0;
+  int raw = analogRead(pin);
+
+  raw < joystickHome - joystickThreshold ? value = 1 : 0;
+  raw > joystickHome + joystickThreshold ? value = -1 : 0;
+
+  return value;
 }
